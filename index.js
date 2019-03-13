4d5911896e24a82ae1d4b3f87c20e7e860a917ca
Yury Korneff <korneff@gmail.com>
2019-03-13 12:01:10 +0100

    in stuck
    stuck step

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let express = require('express');
 
 let app= express();
 app.use('/styles', express.static('public'));
+app.use('/scripts', express.static('public'));
 console.log(__dirname);
 
 let fs = require('fs');
@@ -20,6 +21,14 @@ let https = require('https').Server(credentials,app);
 let io = require('socket.io')(https);
 //let io = require('socket.io')(http);
 
+let rooms = new Map;
+let currentRoom = {
+    id: undefined,
+    owner: undefined,
+    visitorsAmount:0,
+    visitors: []
+};
+
 
 app.get('/', function(req, res){
     res.sendFile(`${__dirname}/index.html` );
@@ -29,8 +38,8 @@ app.get('/chat', function(req, res){
     res.sendFile(`${__dirname}/chat.html` );
 });
 
-app.get('/hw', (req,res) => {
-    res.send('Hello World');
+app.get('/vchat', (req,res) => {
+    res.sendFile(`${__dirname}/vchat.html` );
 });
 
 let sessions=[];
@@ -46,10 +55,16 @@ io.on('connection', function(socket){
         io.to(msg.room).emit('chat message', `${msg.from}: ${msg.text}`);
     });
 
+    socket.on('signal', function(msg){
+        console.log(`Message: ${msg}`);
+        //io.to(msg.room).emit('signal', `${msg.from}: ${msg.text}`);
+        io.to(msg.room).emit('signal', msg);
+    });
+/*
     socket.on('typing message', (session) =>{
         console.log(`${session.userName} is typing now in ${session.room}`);
     });
-
+*/
     socket.on('join chat', function(session){
         sessions.push(session);
         console.log(`Session ID: ${session.id}`);
@@ -59,6 +74,33 @@ io.on('connection', function(socket){
         //console.log(`Room: ${roomId}`);
 
         //console.log(`arr: ${roomId}`);
+    })
+
+    socket.on('enter video room', function(session){
+        sessions.push(session);
+        //console.log(`Session ID: ${session.id}`);
+        //console.log(`Session room: ${session.room}`);
+        //console.log(`Session user: ${session.userName}`);
+       
+        //console.log(rooms[session.room]);
+        currentRoom.name=session.room;
+        if (rooms.has(currentRoom.name)) {
+            rooms.get(currentRoom.name).visitorsAmount++;
+            //rooms.get(currentRoom.name).visitors.push(session.userName);
+            //console.log(rooms.get(currentRoom.name).visitors);
+            console.log(`${session.userName} has joined the room ${currentRoom.name}`);
+            session.owner=false;
+        } else {
+            currentRoom.owner=session.userName;
+            currentRoom.visitorsAmount=1;
+            currentRoom.visitors.push=session.userName;
+            rooms.set(currentRoom.name, currentRoom);
+            console.log(`Room ${currentRoom.name} has been created by ${session.userName}`);
+            session.owner=true;
+        }
+        socket.join(session.room);
+        io.to(session.room).emit('joined', session);        
+
     })
 });
 
